0c1e0fe20cc034bf7d0042d5d91c647e6ae32490
edisonchan97 <edisonzhuozhou@163.com>
2018-01-04 18:13:16 +0800

    忘记密码的文件

---
+++ src/components/Login/ForgetPwd.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="forget-pwd">
+  <!-- <img src="static/index-bg.jpg" alt="" id="index-bg"> -->
+    <div class="container from-group">
+      <h1 id="title">Welcome To Grapro</h1>
+    </div>
+    <div class="container from-group">
+      <div class="input-group col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-3 ">
+        <span class="input-group-addon glyphicon glyphicon-envelope" id=""></span>
+        <input type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
+      </div>
+    </div>
+    <grapro></grapro>  
+  </div>
+</template>
+
+<script>
+import Grapro from "../../components/LittleComponents/Grapro"
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  },
+  components:{Grapro}
+}
+</script>
+
+
+<style scoped>
+  #title{
+    font-size: 3rem;
+    text-align: center;
+    color: #638;
+  }
+  .glyphicon{
+    top: 0;
+  }
+  .input-group{
+    margin-bottom: 10px;
+  }
+
+
+</style>
